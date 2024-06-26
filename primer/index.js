3ad3162d3e70b54b2e6e2a760572228b016f32a9
Marie Robinson <mariethe3rd.dev@gmail.com>
2024-06-26 15:34:42 -0500

    through 4-20

--- primer/index.js
+++ primer/index.js
@@ -1,19 +1,37 @@
-// using classes in JavaSCript 4.18
+// Javascript Primer - 4.3.2 Defining Iterable Objects
 class Product {
   constructor(name, price) {
     this.name = name;
     this.price = price;
   }
+
   toString() {
     return `toString: Name: ${this.name}, Price: ${this.price}`;
   }
 }
 
-function* createProductIterator() {
-  yield new Product("Hat", 100);
-  yield new Product("Boots", 100);
-  yield new Product("Umbrella", 23);
+class GiftPack{
+  constructor(name, prod1, prod2, prod3) {
+    this.name = name;
+    this.prod1 = prod1;
+    this.prod2 = prod2;
+    this.prod3 = prod3;
+  }
 
+  getTotalPrice() {
+    return [this.prod1.price, this.prod2.price, this.prod3.price]
+    .reduce((total,p) => total + p, 0);
+  }
+
+  *getGenerator() {
+    yield this.prod1;
+    yield this.prod2;
+    yield this.prod3;
+  }
 }
 
-[...createProductIterator()].forEach(p => console.log(p.toString()));
+let winter = new GiftPack("winter", new Product("Hat", 100), new Product("Boots", 80), new Product("Gloves", 23));
+
+console.log(`Total price: ${winter.getTotalPrice()}`);
+
+[...winter.getGenerator()].forEach(p => console.log(`Product: ${p}`));
