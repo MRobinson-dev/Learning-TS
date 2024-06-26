eec49d0c2dbba67d295a2915bd343792e17d974a
Marie Robinson <mariethe3rd.dev@gmail.com>
2024-06-26 15:52:51 -0500

    through 4-23

--- primer/index.js
+++ primer/index.js
@@ -1,19 +1,30 @@
 // Javascript Primer - 4.4.2 Storing data by key using a map
-// lisitng 4.22 Using a map in the index.js file in the primer folder
+// lisitng 4.23 Using symbol values as keys in the index.js file in the primer folder
 
 class Product {
   constructor(name, price) {
+    this.id = Symbol();
     this.name = name;
     this.price = price;
   }
+}
 
-  toString() {
-    return `toString: Name: ${this.name}, Price: ${this.price}`;
+class Supplier {
+  constructor(name, productids) {
+    this.name = name;
+    this.productids = productids;
   }
 }
 
-let data = new Map();
-data.set("Hat", new Product("Hat", 100));
-data.set("Boots", new Product("Boots", 100));
+let acmeProducts = [new Product("Hat", 100), new Product("Boots", 100)];
+let zoomProducts = [new Product("Hat", 100), new Product("Boots", 100)];
 
-[...data.keys()].forEach(key => console.log(data.get(key).toString()));
+let products = new Map();
+
+[...acmeProducts, ...zoomProducts].forEach(p => products.set(p.id, p));
+let suppliers = new Map();
+suppliers.set("acme", new Supplier("Acme Co", acmeProducts.map(p => p.id)));
+suppliers.set("zoom", new Supplier("Zoom Shoes", zoomProducts.map(p => p.id)));
+
+suppliers.get("acme").productids.forEach(id => console.log(products.get(id).name));
+
