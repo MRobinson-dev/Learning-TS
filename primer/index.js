98a2e0d31ce361cd2fbd185cf9f21064b9e6bbc4
Marie Robinson <mariethe3rd.dev@gmail.com>
2024-06-26 15:45:41 -0500

    through 4-21

--- primer/index.js
+++ primer/index.js
@@ -1,4 +1,6 @@
 // Javascript Primer - 4.3.2 Defining Iterable Objects
+// lisitng 4.21 - Using an object as a Collection
+
 class Product {
   constructor(name, price) {
     this.name = name;
@@ -10,28 +12,10 @@ class Product {
   }
 }
 
-class GiftPack{
-  constructor(name, prod1, prod2, prod3) {
-    this.name = name;
-    this.prod1 = prod1;
-    this.prod2 = prod2;
-    this.prod3 = prod3;
-  }
-
-  getTotalPrice() {
-    return [this.prod1.price, this.prod2.price, this.prod3.price]
-    .reduce((total,p) => total + p, 0);
-  }
-
-  *[Symbol.iterator]() {
-    yield this.prod1;
-    yield this.prod2;
-    yield this.prod3;
-  }
+let data = {
+  hat: new Product("Hat", 100)
 }
 
-let winter = new GiftPack("winter", new Product("Hat", 100), new Product("Boots", 80), new Product("Gloves", 23));
+data.boots = new Product("Boots", 100);
 
-console.log(`Total price: ${winter.getTotalPrice()}`);
-
-[...winter].forEach(p => console.log(`Product: ${p}`));
+Object.keys(data).forEach(key => console.log(data[key].toString()));
