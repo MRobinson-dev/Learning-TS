b9f150f29ae3e372975d1ecde001a1a0d832390b
Marie Robinson <mariethe3rd.dev@gmail.com>
2024-06-26 15:27:18 -0500

    through 4-17

--- primer/index.js
+++ primer/index.js
@@ -1,43 +1,19 @@
-// using classes in JavaSCript 4.16
+// using classes in JavaSCript 4.18
 class Product {
   constructor(name, price) {
     this.name = name;
     this.price = price;
   }
   toString() {
-    return `toSTring: Name: ${this.name}, Price: ${this.price}`;
+    return `toString: Name: ${this.name}, Price: ${this.price}`;
   }
 }
 
-function createProductIterator() {
-  const hat = new Product("Hat", 100);
-  const boots = new Product("Boots", 100);
-  const umbrella = new Product("Umbrella", 23);
+function* createProductIterator() {
+  yield new Product("Hat", 100);
+  yield new Product("Boots", 100);
+  yield new Product("Umbrella", 23);
 
-  let lastVal;
-
-  return {
-    next() {
-      switch (lastVal) {
-        case undefined:
-          lastVal = hat;
-          return { value: hat, done: false };
-        case hat:
-          lastVal = boots;
-          return { value: boots, done: false };
-        case boots:
-          lastVal = umbrella;
-          return { value: umbrella, done: false };
-        case umbrella:
-          return { value: undefined, done: true };
-      }
-    }
-  }
 }
 
-let iterator = createProductIterator();
-let result = iterator.next();
-while (!result.done) {
-  console.log(result.value.toString());
-  result = iterator.next();
-}
+[...createProductIterator()].forEach(p => console.log(p.toString()));
